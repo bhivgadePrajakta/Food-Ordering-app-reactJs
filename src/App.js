0847b75b76ec6c537640d36525aecae1cc409d00
Prajakta <prajaktabhivagade@gmail.com>
2024-02-10 14:13:18 +0530

    [FIX]: build issues

--- src/App.js
+++ src/App.js
@@ -1,52 +1,50 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider,Outlet } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 
-
-    
-const AppLayout = () =>{
+const AppLayout = () => {
   return (
-      <div className="app">
-  
-          <Header/>
-          <Outlet/>
-      </div>
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
   );
 };
 
 const appRouter = createBrowserRouter([
   {
-    path:"/",
-    element:<AppLayout/>,
-    children:[
+    path: "/",
+    element: <AppLayout />,
+    children: [
       {
-        path:"/",
-        element:<Body/>
+        path: "/",
+        element: <Body />,
       },
       {
-      path:"/about",
-      element:<About/>
-    },
-    {
-      path: "/contact",
-      element:<Contact/>
-    },
-    {
-      path: "/restaurants/:resId",
-      element:<RestaurantMenu/>
-    }
-  ],
-    errorElement:<Error/>
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
   },
-  
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<RouterProvider router = {appRouter}/>);
+export const App = ()=> {
+  return <RouterProvider router={appRouter} />;
+}
